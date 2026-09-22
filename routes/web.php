9d86e2d482c402effbb463a3bfda8c6ef82088c6
Ing Brayan Leon <48490070+leon9612@users.noreply.github.com>
2026-09-21 20:05:19 -0500

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,134 @@
+<?php
+
+
+use App\Http\Controllers\Cvh;
+use App\Http\Controllers\Cal;
+use App\Http\Controllers\Csuspension;
+use App\Http\Controllers\Csonometro;
+use App\Http\Controllers\Ctaximetro;
+use App\Http\Controllers\Cfrenometro;
+use App\Http\Controllers\Copacidad;
+use App\Http\Controllers\Clogin;
+use App\Http\Controllers\Cgases;
+use App\Http\Controllers\Cgasesmotos;
+use App\Http\Controllers\Cluces;
+use App\Http\Controllers\Clucesmotos;
+use App\Http\Controllers\Cprincipal;
+use App\Http\Controllers\Cfrenomotos;
+use App\Http\Controllers\Cfrenomotocarro;
+use App\Http\Controllers\Cvisual;
+use App\Http\Controllers\Cactualizar;
+use App\Http\Controllers\Calibracion;
+use App\Http\Controllers\Cfotos;
+use App\Traits\EventosTrait;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::resource('/', Clogin::class);
+Route::resource('al', Cal::class);
+Route::resource('su', Csuspension::class);
+Route::resource('op', Copacidad::class);
+Route::resource('so', Csonometro::class);
+Route::resource('tax', Ctaximetro::class);
+Route::resource('fr', Cfrenometro::class);
+Route::resource('frm', Cfrenomotos::class);
+Route::resource('ga', Cgases::class);
+Route::resource('gam', Cgasesmotos::class);
+Route::resource('lu', Cluces::class);
+Route::resource('lum', Clucesmotos::class);
+Route::resource('cpr', Cprincipal::class);
+Route::resource('frmotocarro', Cfrenomotocarro::class);
+Route::resource('visual', Cvisual::class);
+Route::resource('update', Cactualizar::class);
+Route::resource('cal', Calibracion::class);
+Route::resource('fot', Cfotos::class);
+
+Route::get('/close', [Clogin::class, 'cerrarSesion']);
+Route::post('/buscarvehiculo', [Cprincipal::class, 'getVehiculo']);
+
+Route::post('/getMaquina', [Cprincipal::class, 'getMaquina']);
+Route::post('/getActualizacion', [Cactualizar::class, 'getActualizacion']);
+Route::post('/getCalibracion', [Calibracion::class, 'getCalibracion']);
+
+Route::post('/consultarImagen', [Cfotos::class, 'consultarImagen']);
+
+Route::post('/getevento', [Cprincipal::class, 'eventosindra']);
+Route::post('/getlineas', [Cprincipal::class, 'getlineas']);
+Route::get('/getmac', [Clogin::class, 'getMac']);
+// Route::get('index.php/getSession', [Clogin::class, 'getSession']);
+Route::get('/getSession', [Clogin::class, 'getSession']);
+Route::post('/getDefectos', [Cvisual::class, 'getDefectos']);
+Route::post('/updateObservacion', [Cvisual::class, 'updateObservacion']);
+Route::post('/saveObservacionAdicional', [Cvisual::class, 'saveObservacionAdicional']);
+Route::post('/deleteDefectos', [Cvisual::class, 'deleteDefectos']);
+Route::post('/saveDefectos', [Cvisual::class, 'saveDefectos']);
+Route::post('/saveLabrado', [Cvisual::class, 'saveLabrado']);
+//Route::resource('principal', Cprincipal::class);
+
+Route::post('/getPlacas', [EventosTrait::class, 'getPlacas']);
+
+
+Route::post('/getPlacasByTipo', [Cprincipal::class, 'getPlacasByTipo']);
+Route::get('/getPlacasByTipo/{tipoejecucion}', [Cprincipal::class, 'getPlacasByTipo']);
+
+Route::get('/get-archivo-desencriptado', [Cprincipal::class, 'getArchivoDesencriptado']);
+
+// Route::post('/proxy-analizador', function (Request $request) {
+//     $tipoprueba = $request->post('tipoprueba');
+//     unset($request['tipoprueba']);
+//     try {
+//         $response = Http::withHeaders([
+//             'Content-Type' => 'application/json',
+//         ])->post('http://localhost:3000/' . $tipoprueba . '/escribir-datos', $request->all());
+
+//         return response()->json($response->json(), $response->status());
+//     } catch (\Exception $e) {
+//         return response()->json(['error' => $e->getMessage()], 500);
+//     }
+// })->name('proxy.analizador');
+
+
+// Route::post('/proxy-detener-escritura', function (Request $request) {
+//     $tipoprueba = $request->post('tipoprueba');
+//     unset($request['tipoprueba']);
+//     try {
+//         $response = Http::withHeaders([
+//             'Content-Type' => 'application/json',
+//         ])->post('http://localhost:3000/' . $tipoprueba . '/detener-escritura', $request->all());
+
+//         return response()->json($response->json(), $response->status());
+//     } catch (\Exception $e) {
+//         return response()->json(['error' => $e->getMessage()], 500);
+//     }
+// })->name('proxy.detener.escritura');
+
+// Route::post('/proxy-iniciar-captador', function (Request $request) {
+//     $tipoprueba = $request->post('tipoprueba');
+//     unset($request['tipoprueba']);
+//     // echo "Datos recibidos en Laravel: " . json_encode($request->all()) . "\n";
+
+//     try {
+//         $response = Http::withHeaders([
+//             'Content-Type' => 'application/json',
+//         ])->post('http://localhost:3000/' . $tipoprueba . '/iniciar-captador', $request->all());
+
+//         return response()->json($response->json(), $response->status());
+//     } catch (\Exception $e) {
+//         return response()->json(['error' => $e->getMessage()], 500);
+//     }
+// })->name('proxy.iniciar.captador');
